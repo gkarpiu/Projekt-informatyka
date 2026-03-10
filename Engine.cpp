4ced19488c5bfa3435ea232dd930f50dba14b3c7
gkarpiu <gkarpowi10@gmail.com>
2026-03-10 07:36:11 +0100

    hgfl

--- Engine.cpp
+++ Engine.cpp
@@ -42,7 +42,7 @@ unsigned int modelLoc;
 unsigned int viewLoc;
 unsigned int projLoc;
 unsigned int shaderProgram;
-bool firstMouse;
+bool firstMouse=1;
 float lastX, lastY;
 std::vector<Renderer> meshes(1);
 std::vector<Entity> entities;
@@ -277,7 +277,7 @@ int InitEngine(){
     viewLoc=glGetUniformLocation(shaderProgram, "view");
     projLoc=glGetUniformLocation(shaderProgram, "projection");
 
-    Texture texture=LoadTexture("kotek2.png");
+    Texture texture=LoadTexture("concrete.png");
     glUseProgram(shaderProgram);
     glUniform1i(glGetUniformLocation(shaderProgram, "tex"), 0);
     glViewport(0, 0, WINDOW_WIDTH*WINDOW_SCALE, WINDOW_HEIGHT*WINDOW_SCALE);
