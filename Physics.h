b6198be957136116c7206e39a1e832ff5bc01eac
Hexxorr1 <filipkoltuniuk@gmail.com>
2026-03-13 23:14:52 +0100

    smol changez

--- Physics.h
+++ Physics.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <glm/glm.hpp>
-#include "Camera.h"
 #include "Engine.h"
 
 struct AABB{
--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ int main(){
     size_t texConcrete=LoadTexture("concrete.png");
     size_t texWood=LoadTexture("wood.png");
     size_t texPlastic=LoadTexture("plastic.png");
+    size_t texMetal=LoadTexture("metal.png");
 
     size_t mesh1=LoadObject("thatthing.obj");
 
-    size_t e1=AddEntity(mesh1, mesh1, texConcrete, 0);
+    size_t e1=AddEntity(mesh1, mesh1, texMetal, 0);
     size_t e2=AddEntity(mesh1, mesh1, texWood, 0);
 
     entities[e1].transform=glm::translate(entities[e1].transform, {10, 10, 10});
@@ -20,9 +21,6 @@ int main(){
     //Render loop
     while(!ShouldClose())
     {
-        //entities[e1].transform=glm::rotate(entities[e1].transform, 0.01f, {0, 1, 0});
-        //entities[e2].transform=glm::rotate(entities[e2].transform, 0.01f, {-1, 0, 0});
-
         DoMovement(camera, window);
         std::vector<size_t> triggerIds;
         CheckTriggers(camera, triggerIds);
